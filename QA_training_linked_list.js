2d16481fccd961952cb89cfd92b81ca36296f5a7
Maria <m.gribacheva@gmail.com>
2019-12-25 21:08:42 -0800

    removal method done

--- QA_training_linked_list.js
+++ QA_training_linked_list.js
@@ -20,6 +20,40 @@ class LinkedList{
     }
   };
 
+  remove(val){
+
+    let node = this.head;
+    if(node === null){ // if there's no nodes, just a head (which equals null)
+      return;
+    }
+    let prevNode = null;
+    while(node && node.next != null){
+      if(node.value === val){ // if we need to remove a node
+        if(prevNode === null){ // if it's the first node in a row
+          this.head = node.next; 
+          node = node.next;
+        }else{ // if it has a previous one
+          prevNode.next = node.next;
+          node = node.next;
+        }
+        
+      }else{
+        prevNode = node;
+        node = node.next; // move one node further
+      }
+   
+    }
+
+    if(node && node.value === val){ // if node.next is the tail, the last node
+      if(prevNode != null){
+        prevNode.next = null;
+      }else{
+        this.head = null
+      }
+    }
+
+  }
+
   get(){
     let arr = [];
     let tail = this.head;
@@ -50,10 +84,6 @@ class LinkedList{
     }
     return count;
   }
-  remove(){
-
-  }
-
 }
 
 class Node{
@@ -64,22 +94,67 @@ class Node{
   }
 }
 
+// console.log(a);
+// console.log(a.size());
+// console.log(a.get());
+
+// a.add('first');
+// console.log(a);
+// console.log(a.size());
+// console.log(a.get());
+
+// a.add('second');
+// console.log(a);
+// console.log(a.size());
+// console.log(a.get());
+
+// a.add('third');
+// console.log(a);
+// console.log(a.size());
+// console.log(a.get());
 let a = new LinkedList();
-console.log(a);
-console.log(a.size());
-console.log(a.get());
 
 a.add('first');
-console.log(a);
-console.log(a.size());
-console.log(a.get());
-
 a.add('second');
-console.log(a);
-console.log(a.size());
+a.add('third');
+console.log(a.get());
+a.remove('second')
 console.log(a.get());
 
-a.add('trird');
-console.log(a);
-console.log(a.size());
+a = new LinkedList();
+a.add('first');
+a.add('second');
+a.add('third');
+a.add('first');
+console.log(a.get());
+a.remove('first')
+console.log(a.get());
+
+a = new LinkedList();
+a.add('first');
+console.log(a.get());
+a.remove('first')
+console.log(a.get());
+
+a = new LinkedList();
+a.add('first');
+a.add('first');
+a.add('first');
+a.add('first');
+console.log(a.get());
+a.remove('first')
+
+console.log(a.get());
+
+a = new LinkedList();
+console.log(a.get());
+a.remove('first')
+console.log(a.get());
+
+a = new LinkedList();
+a.add('first');
+a.add('second');
+
+console.log(a.get());
+a.remove('first')
 console.log(a.get());
